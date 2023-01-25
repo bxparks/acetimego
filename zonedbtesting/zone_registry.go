aab130c7b7077a9d771b7674e8bb7db265225006
Brian Park <brian@xparks.net>
2023-01-25 08:13:47 -0800

    zonedb*: Add ZoneInfoIndexXxx constants for unit tests

--- zonedbtesting/zone_registry.go
+++ zonedbtesting/zone_registry.go
@@ -57,7 +57,11 @@ var Context = zoneinfo.ZoneContext{
 }
 
 // ---------------------------------------------------------------------------
-// Zone IDs
+// Zone IDs. Unique stable uint32 identifier for each zone which can be given to
+// ZoneManager.NewTimeZoneFromID(). Useful for microcontroller environments
+// where saving variable length strings is more difficult than a fixed width
+// integer.
+//
 // Total: 5 (4 zones, 1 links)
 // ---------------------------------------------------------------------------
 
@@ -67,5 +71,21 @@ const (
 	ZoneIDEtc_UTC uint32 = 0xd8e31abc // Etc/UTC
 	ZoneIDPacific_Apia uint32 = 0x23359b5e // Pacific/Apia
 	ZoneIDUS_Pacific uint32 = 0xa950f6ab // US/Pacific
+
+)
+
+// ---------------------------------------------------------------------------
+// Zone Indexes. Index into the ZoneInfos array. Intended for unit tests
+// which need direct access to the zoneinfo.ZoneInfo struct.
+//
+// Total: 5 (4 zones, 1 links)
+// ---------------------------------------------------------------------------
+
+const (
+	ZoneInfoIndexAmerica_Los_Angeles uint16 = 3 // America/Los_Angeles
+	ZoneInfoIndexAmerica_New_York uint16 = 0 // America/New_York
+	ZoneInfoIndexEtc_UTC uint16 = 4 // Etc/UTC
+	ZoneInfoIndexPacific_Apia uint16 = 1 // Pacific/Apia
+	ZoneInfoIndexUS_Pacific uint16 = 2 // US/Pacific
 
 )
