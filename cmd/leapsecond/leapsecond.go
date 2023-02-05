de1ab37ff9f1c19e25464eb1390780f9ccae5d3e
Brian Park <brian@xparks.net>
2023-02-05 15:51:23 -0800

    leapsecond: See how Go/time or AceTimeGo/acetime handles leap seconds

---
+++ cmd/leapsecond/leapsecond.go
@@ -0,0 +1,104 @@
+// Sample program to determine how Golang time package handles leap seconds.
+//
+// The package document explicitly says that leap seconds are not handled. Since
+// the Time type stores only the number of seconds, it would be unable to
+// distinguish between 23:59:59 and 23:59:60 during a leap second.
+// It seems to normalize the 2016-12-31T23:59:60 into 2017-01-01T00:00:00, which
+// makes sense because Time stores only the epochSeconds component.
+//
+// In AceTimeGo/acetime, the ZonedDateTime struct stores the broken down
+// date-time components, so in theory it may support leap seconds. However,
+// AceTimeGo also explicitly does not support leap seconds. The retains the
+// broken down 23:59:60 time, but when converted to epochSeconds, it returns a
+// value identical to 00:00:00.
+//
+// $ go run leapsecond.go
+// ==== 2016 Leap second by Go time package
+// 2016-12-31 23:59:59 +0000 UTC ; seconds= 1483228799
+// 2017-01-01 00:00:00 +0000 UTC ; seconds= 1483228800
+// 2017-01-01 00:00:00 +0000 UTC ; seconds= 1483228800
+// ==== 2016 Leap second by acetime package
+// 2016-12-31T23:59:59+00:00[UTC] ; seconds= 1483228799
+// 2016-12-31T23:59:60+00:00[UTC] ; seconds= 1483228800
+// 2017-01-01T00:00:00+00:00[UTC] ; seconds= 1483228800
+
+package main
+
+import (
+	"github.com/bxparks/AceTimeGo/acetime"
+	"github.com/bxparks/AceTimeGo/zonedb"
+	"time"
+)
+
+const (
+	name = "UTC"
+	// name = "America/Los_Angeles"
+)
+
+func main() {
+	leapGoTime()
+	leapAceTime()
+}
+
+func leapAceTime() {
+	println("==== 2016 Leap second by acetime package")
+	context := &zonedb.DataContext
+	zm := acetime.NewZoneManager(context)
+	atz := zm.NewTimeZoneFromName(name)
+	if atz.IsError() {
+		println("ERROR: acetime package: Zone", name, "not found")
+		return
+	}
+
+	ldt := acetime.LocalDateTime{2016, 12, 31, 23, 59, 59, 0 /*Fold*/}
+	zdt := acetime.NewZonedDateTimeFromLocalDateTime(&ldt, &atz)
+	if zdt.IsError() {
+		println("ERROR: ", name, ": Unable to create ZonedDateTime for ",
+			ldt.String())
+		return
+	}
+	seconds := zdt.EpochSeconds()
+	println(zdt.String(), "; seconds=", seconds)
+
+	ldt = acetime.LocalDateTime{2016, 12, 31, 23, 59, 60, 0 /*Fold*/}
+	zdt = acetime.NewZonedDateTimeFromLocalDateTime(&ldt, &atz)
+	if zdt.IsError() {
+		println("ERROR: ", name, ": Unable to create ZonedDateTime for ",
+			ldt.String())
+		return
+	}
+	seconds = zdt.EpochSeconds()
+	println(zdt.String(), "; seconds=", seconds)
+
+	ldt = acetime.LocalDateTime{2017, 1, 1, 0, 0, 0, 0 /*Fold*/}
+	zdt = acetime.NewZonedDateTimeFromLocalDateTime(&ldt, &atz)
+	if zdt.IsError() {
+		println("ERROR: ", name, ": Unable to create ZonedDateTime for ",
+			ldt.String())
+		return
+	}
+	seconds = zdt.EpochSeconds()
+	println(zdt.String(), "; seconds=", seconds)
+}
+
+func leapGoTime() {
+	println("==== 2016 Leap second by Go time package")
+
+	gotz, err := time.LoadLocation(name)
+	if err != nil {
+		println("ERROR: time package: Zone", name, "not found")
+		return
+	}
+
+	gotime := time.Date(2016, 12, 31, 23, 59, 59, 0, gotz)
+	seconds := gotime.Unix()
+	println(gotime.String(), "; seconds=", seconds)
+
+	gotime = time.Date(2016, 12, 31, 23, 59, 60, 0, gotz)
+	seconds = gotime.Unix()
+	println(gotime.String(), "; seconds=", seconds)
+
+	gotime = time.Date(2017, 1, 1, 0, 0, 0, 0, gotz)
+	seconds = gotime.Unix()
+	println(gotime.String(), "; seconds=", seconds)
+}
