6f285c57ecab4dee6de4af30f4b7aee4fbe82b90
Brian Park <brian@xparks.net>
2023-02-05 18:20:57 -0800

    cmd/validatetime: Extract DST transitions for each zone and validate before/after; validate samples from [1974,2150)

--- cmd/validatetime/validatetime.go
+++ cmd/validatetime/validatetime.go
@@ -13,6 +13,7 @@ import (
 const (
 	startYear = 1974
 	untilYear = 2150
+	samplingInterval = 22 // hours
 )
 
 func main() {
@@ -40,64 +41,94 @@ func validateZoneName(zm *acetime.ZoneManager, name string) {
 		return
 	}
 
-	var year int16
-	var month uint8
-	var day uint8
-	for year = startYear; year < untilYear; year++ {
-		for month = 1; month <= 12; month++ {
-			for day = 1; day <= 28; day++ {
-				// Create acetime.ZonedDateTime
-				ldt := acetime.LocalDateTime{year, month, day, 2, 3, 4, 0 /*Fold*/}
-				zdt := acetime.NewZonedDateTimeFromLocalDateTime(&ldt, &atz)
-				if zdt.IsError() {
-					println("ERROR: ", name, ": Unable to create ZonedDateTime for ",
-						ldt.String())
-					return
-				}
+	// Validate the before and after DST transitions.
+	transitions := findTransitions(startYear, untilYear, samplingInterval, stz)
+	for _, transition := range transitions {
+		validateAtTime(transition.before, &atz)
+		validateAtTime(transition.after, &atz)
+	}
+	println("\tNum Transitions:", len(transitions))
 
-				// Create time.Time
-				unixSeconds64 := int64(zdt.EpochSeconds())
-				st := time.Unix(unixSeconds64, 0 /*nanos*/).In(stz)
-
-				// Validate components. The time.Time struct holds just a counter
-				// (nanoseconds since a specific date-time), and the timezone. It does
-				// NOT hold the date-time components like acetime.ZonedDateTime.
-				// Therefore, each call to one of the component methods (e.g. Year(),
-				// Month(), etc) causes a conversion from this counter to the
-				// human-readable date-time components, which is a relatively slow
-				// process. Each of the following if-statement causes the program to
-				// become slower and slower.
-				if int16(st.Year()) != zdt.Year {
-					println("ERROR: ", name, ": Year not equal: ",
-						st.String(), zdt.String())
-					return
-				}
-				if uint8(st.Month()) != zdt.Month {
-					println("ERROR: ", name, ": Month not equal: ",
-						st.String(), zdt.String())
-					return
-				}
-				if uint8(st.Day()) != zdt.Day {
-					println("ERROR: ", name, ": Day not equal: ",
-						st.String(), zdt.String())
-					return
-				}
-				if uint8(st.Hour()) != zdt.Hour {
-					println("ERROR: ", name, ": Hour not equal: ",
-						st.String(), zdt.String())
-					return
-				}
-				if uint8(st.Minute()) != zdt.Minute {
-					println("ERROR: ", name, ": Minute not equal: ",
-						st.String(), zdt.String())
-					return
-				}
-				if uint8(st.Second()) != zdt.Second {
-					println("ERROR: ", name, ": Second not equal: ",
-						st.String(), zdt.String())
-					return
-				}
+	// Validate some samples
+	samples := 0
+	for year := startYear; year < untilYear; year++ {
+		for month := 1; month <= 12; month++ {
+			for day := 1; day <= 28; day++ {
+				gt := time.Date(year, time.Month(month), day, 2, 0, 0, 0, stz)
+				validateAtTime(gt, &atz)
+				samples++
 			}
 		}
+	}
+	println("\tNum Samples:", samples)
+}
+
+func validateAtTime(t time.Time, atz *acetime.TimeZone) {
+	name := atz.Name()
+
+	// Create acetime.ZonedDateTime based on the EpochSeconds, which uniquely
+	// identifies the time. We can't use the components because those can be
+	// ambiguous during an overlap.
+	unixSeconds := t.Unix()
+	zdt := acetime.NewZonedDateTimeFromEpochSeconds(
+		acetime.ATime(unixSeconds), atz)
+	if zdt.IsError() {
+		println("ERROR: ", name, ": Unable to create ZonedDateTime for ",
+			unixSeconds)
+		return
+	}
+
+	year := int16(t.Year())
+	month := uint8(t.Month())
+	day := uint8(t.Day())
+	hour := uint8(t.Hour())
+	minute := uint8(t.Minute())
+	second := uint8(t.Second())
+
+	// Validate UnixSeconds. This should always succeed.
+	acetimeEpochSeconds := int64(zdt.EpochSeconds())
+	if acetimeEpochSeconds != unixSeconds {
+		println("ERROR: ", name, ": EpochSeconds not equal",
+			acetimeEpochSeconds, unixSeconds)
+		return
+	}
+
+	// Validate components. The time.Time struct holds just a counter
+	// (nanoseconds since a specific date-time), and the timezone. It does
+	// NOT hold the date-time components like acetime.ZonedDateTime.
+	// Therefore, each call to one of the component methods (e.g. Year(),
+	// Month(), etc) causes a conversion from this counter to the
+	// human-readable date-time components, which is a relatively slow
+	// process. Each of the following if-statement causes the program to
+	// become slower and slower.
+	if int16(year) != zdt.Year {
+		println("ERROR: ", name, ": Year not equal: ",
+			t.String(), zdt.String())
+		return
+	}
+	if uint8(month) != zdt.Month {
+		println("ERROR: ", name, ": Month not equal: ",
+			t.String(), zdt.String())
+		return
+	}
+	if uint8(day) != zdt.Day {
+		println("ERROR: ", name, ": Day not equal: ",
+			t.String(), zdt.String())
+		return
+	}
+	if uint8(hour) != zdt.Hour {
+		println("ERROR: ", name, ": Hour not equal: ",
+			t.String(), zdt.String())
+		return
+	}
+	if uint8(minute) != zdt.Minute {
+		println("ERROR: ", name, ": Minute not equal: ",
+			t.String(), zdt.String())
+		return
+	}
+	if uint8(second) != zdt.Second {
+		println("ERROR: ", name, ": Second not equal: ",
+			t.String(), zdt.String())
+		return
 	}
 }
