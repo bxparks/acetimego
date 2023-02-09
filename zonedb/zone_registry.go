998dcfa4a9617a0a54e6d48048e696f3bec706c6
Brian Park <brian@xparks.net>
2023-02-09 08:42:40 -0800

    zonedb*: Add documentation for DataContext and RecordContext from AceTimeTools/gogenerator.py

--- zonedb/zone_registry.go
+++ zonedb/zone_registry.go
@@ -39,6 +39,14 @@ import (
 
 const TzDatabaseVersion string = "2022g"
 
+// RecordContext contains references to the various arrays of ZoneRuleRecord,
+// ZonePolicyRecord, ZoneEraRecord, and ZoneInfoRecord objects, as well as the
+// strings used by those objects.
+//
+// The `acetime` package uses the encoded XxxData objects, not the XxxRecord
+// objects referenced here. These XxxRecord objects are used only for testing
+// purposes, to verify that the XxxData objects were properly generated, and can
+// be read back and reconstructed to be identical to the XxxRecord objects.
 var RecordContext = zoneinfo.ZoneRecordContext{
 	TzDatabaseVersion: TzDatabaseVersion,
 	StartYear: 1974,
@@ -55,6 +63,13 @@ var RecordContext = zoneinfo.ZoneRecordContext{
 	ZoneInfoRecords: ZoneInfoRecords,
 }
 
+// DataContext contains references to various XxxData objects and strings. These
+// are the binary encoded versions of the various XxxRecord objects. This object
+// is passed to the ZoneManager.
+//
+// The encoding to a binary string is performed because the Go language is able
+// to treat strings as constants, and the TinyGo compiler can place them in
+// flash memory, saving tremendous amounts of random memory.
 var DataContext = zoneinfo.ZoneDataContext{
 	TzDatabaseVersion: TzDatabaseVersion,
 	StartYear: 1974,
