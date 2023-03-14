ad27dca2c01003a6f6f6fb8c8ae8db0f7077d5f9
Brian Park <brian@xparks.net>
2023-03-14 14:44:58 -0700

    zonedb2000: zonedb for year 2000 and onwards only

---
+++ cmd/mem_zonedb2000/mem_zonedb2000.go
@@ -0,0 +1,58 @@
+// Print the memory usage of acetime library when using the zonedb2000 database.
+//
+//$ go run mem_zone_registry.go
+
+package main
+
+import (
+	"github.com/bxparks/AceTimeGo/acetime"
+	"github.com/bxparks/AceTimeGo/strbuild"
+	"github.com/bxparks/AceTimeGo/zonedb2000"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	print("---- Initial memory usage\n")
+	PrintMemUsage()
+
+	print("---- Create ZonedDateTime using zonedb2000\n")
+	zm := acetime.NewZoneManager(&zonedb2000.DataContext)
+	name := "America/Los_Angeles"
+	tz := zm.TimeZoneFromName(name)
+	if tz.IsError() {
+		print("ERROR: Could not find TimeZone for ")
+		print(name)
+		print("\n")
+	}
+	ldt := acetime.LocalDateTime{2023, 1, 19, 18, 36, 0, 0 /*Fold*/}
+	zdt := acetime.NewZonedDateTimeFromLocalDateTime(&ldt, &tz)
+	print("zdt:")
+	print(zdt.String())
+	print("\n")
+	PrintMemUsage()
+
+	print("---- Run GC()\n")
+	runtime.GC()
+	PrintMemUsage()
+}
+
+func PrintMemUsage() {
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	var b strings.Builder
+	print("TotalAlloc = ")
+	strbuild.Uint64(&b, m.TotalAlloc)
+	print(b.String())
+	print("\tSys = ")
+	b.Reset()
+	strbuild.Uint64(&b, m.Sys)
+	print(b.String())
+	print("\n")
+
+	// These are not found on tinygo.
+	//fmt.Printf("Alloc = %v B", m.Alloc)
+	//fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
